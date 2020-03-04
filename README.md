6a6d19b78330de096280b801d7a7705f161985eb
Arba Sasmoyo <arba.sasmoyo@gmail.com>
2020-03-04 20:58:39 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Info MOS (Indonesia Macet of Shame) - BETA
+==========================================
+
+Aplikasi simpel yang mengcapture kemacetan lalu lintas dengan menggunakan Google Map.
+
+# Fitur
+
+Untuk saat ini Indo MOS masih dalam tahap brainstorming dan pengembangan awal. Akan tetapi akan diusahakan untuk membuat Web UI yang dapat digunakan untuk melihat komparasi lalu lintas dari waktu ke waktu. Harapannya data lalu lintas yang didapat bisa digunakan untuk membantu mengurangi kemacetan dikemudian hari™.
+
+# Kontribusi
+
+Kontribusi kamu sangat ditunggu! Kamu bisa menambah lokasi tempat macet yang ingin kamu rekam datanya dengan menambah data di [locations](locations). Selain itu ide dan saran mengenai pengembangan aplikasi ini juga diharapkan :)
---
+++ index.js
@@ -0,0 +1,60 @@
+const puppeteer = require('puppeteer')
+const glob = require('glob')
+const fs = require('fs')
+const moment = require('moment')
+
+const getLocations = () => {
+    return new Promise((resolve, reject) => {
+        glob('locations/*.json', function (err, files) {
+            if (err) {
+                reject(err)
+            }
+            else {
+                resolve(files)
+            }
+        })
+    })
+}
+
+const capture = (lat, lon, zoom, output) => {
+    return new Promise(async (resolve, reject) => {
+        const URL = `https://www.google.com/maps/@?api=1&map_action=map&center=${lat},${lon}&zoom=${zoom}&layer=traffic`
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+
+        await page.goto(URL)
+        await page.screenshot({ path: output })
+        await browser.close()
+    })
+}
+
+const normalizeName = name => {
+    return name.toLowerCase().replace(' ', '_').replace(/[^a-z-_]/, '')
+}
+
+const parseCityName = fileName => {
+    return fileName.replace('locations/', '').replace('.json', '')
+}
+
+getLocations()
+    .then(async (locations) => {
+        var promises = []
+        locations.forEach(location => {
+            var contents = fs.readFileSync(location);
+            var jsonContent = JSON.parse(contents);
+
+            const city = parseCityName(location)
+            if (! fs.existsSync(`data/${city}`)) {
+                fs.mkdirSync(`data/${city}`)
+            }
+
+            Object.keys(jsonContent).forEach(key => {
+                const lat = jsonContent[key].lat
+                const lon = jsonContent[key].lon
+                const zoom = (jsonContent[key].zoom) ? jsonContent[key].zoom : 18
+                const output = `data/${city}/${normalizeName(key)}_${moment().format('YYYY-MM-DD_hh:mm')}.png`
+                promises.push(capture(lat, lon, zoom, output))
+            })
+        })
+        await Promise.all(promises)
+    })
